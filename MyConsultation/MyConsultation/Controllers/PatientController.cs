68d626185ee096231ceb37bbfbf3553fee14f432
ArtemNemtsov <nemcov_a@mail.ru>
2020-06-29 03:09:33 +0500

    -- первый коммит

---
+++ MyConsultation/MyConsultation/Controllers/PatientController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyConsultation.Controllers
+{
+    public class PatientController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
